d858ffe0c787d6bdcb9bc98b2c40bcbd46e8f941
NishiyaKN <udonge@proton.me>
2025-05-30 13:00:02 -0300

    Update config files

--- scripts/baka.sh
+++ scripts/baka.sh
@@ -3,3 +3,4 @@
 ~/re/git/dotfiles/scripts/ura.sh
 ~/re/git/dotfiles/scripts/stu.sh
 ~/re/git/dotfiles/scripts/git.sh
+~/re/git/dotfiles/scripts/rpi.sh
---
+++ scripts/rpi.sh
@@ -0,0 +1,4 @@
+cd /home/yori/re/git/rpi
+git add .
+git commit -m 'Upload files'
+git push origin main
