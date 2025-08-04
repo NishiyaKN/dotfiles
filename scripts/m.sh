37d76d7114e095d95a17c0af11acaee37bea4a4a
NishiyaKN <udonge@proton.me>
2025-08-04 00:00:04 -0300

    Update config files

---
+++ scripts/m.sh
@@ -0,0 +1,14 @@
+# Louvor
+python3 ~/yt-dlp -f bestaudio -ciw -o '~/Music/ma/%(playlist_title)s/%(title)s.%(ext)s' -x --audio-format mp3 --embed-thumbnail --add-metadata --download-archive '~/.config/ytdl/yoriko.txt' --cookies '~/re/git/dotfiles/config/ytdl/cookies-youtube-com.txt' 'https://youtube.com/playlist?list=PLroxs_DzNNxigqVyPoZQd021YMGHc6Rok&si=dAkPIjCD49XUG8Ah'
+
+# Nihon
+python3 ~/yt-dlp -f bestaudio -ciw -o '~/Music/ma/%(playlist_title)s/%(title)s.%(ext)s' -x --audio-format mp3 --embed-thumbnail --add-metadata --download-archive '~/.config/ytdl/yoriko.txt' --cookies '~/re/git/dotfiles/config/ytdl/cookies-youtube-com.txt' 'https://youtube.com/playlist?list=PLroxs_DzNNxgwSmeSXLlDs4JmBB3ih_Xd&si=ann6fC3NQrJ7zD2f'
+
+# Ingles
+python3 ~/yt-dlp -f bestaudio -ciw -o '~/Music/ma/%(playlist_title)s/%(title)s.%(ext)s' -x --audio-format mp3 --embed-thumbnail --add-metadata --download-archive '~/.config/ytdl/yoriko.txt' --cookies '~/re/git/dotfiles/config/ytdl/cookies-youtube-com.txt' 'https://youtube.com/playlist?list=PLroxs_DzNNxgCxRXk29OnEj977pJTJQ6f&si=QEAjsoLE1oHgDQTw'
+
+# Nihon
+python3 ~/yt-dlp -f bestaudio -ciw -o '~/Music/ma/%(playlist_title)s/%(title)s.%(ext)s' -x --audio-format mp3 --embed-thumbnail --add-metadata --download-archive '~/.config/ytdl/yoriko.txt' --cookies '~/re/git/dotfiles/config/ytdl/cookies-youtube-com.txt' 'https://youtube.com/playlist?list=PLroxs_DzNNxiLPRoFxXtUGvDEmdzW-PEG&si=fi5N0tQdS1yRMCB7'
+
+# Espanhol
+python3 ~/yt-dlp -f bestaudio -ciw -o '~/Music/ma/%(playlist_title)s/%(title)s.%(ext)s' -x --audio-format mp3 --embed-thumbnail --add-metadata --download-archive '~/.config/ytdl/yoriko.txt' --cookies '~/re/git/dotfiles/config/ytdl/cookies-youtube-com.txt' 'https://youtube.com/playlist?list=PLroxs_DzNNxgUTbAcHjEjVGp_CdOr5AVF&si=CA2eU6k_uLnRaiMu'
